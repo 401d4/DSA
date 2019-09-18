14a2d8ec70f47953ea9593d2699e561bdcae54bb
Jacob-Wendt <jacob.wendt@yhaoo.com>
2019-09-18 15:08:26 -0500

    added tests and function

--- arrayBinarySearch/array-binary-search.js
+++ arrayBinarySearch/array-binary-search.js
@@ -1,18 +1,31 @@
 'use strict';
 
-let binarySearch = (array,key) => {
-  for (let index = 0; index < array.length; index++) {
-    console.log('the key is ',key,' the index value is ',array[index]);
-    if (key === array[index]) {return index;}
-    else {continue;}
+
+let binarySearch = (array,key) => { 
+  let middle = Math.floor(array.length/2);
+  let left = 0;
+  let right = array.length -1;
+
+  while(key !== array[middle]) {
+    // console.log('the middle is ', middle);
+    // console.log('the left is ', left);
+    // console.log('the right is ', right);
+    if (left === right && right === middle) {
+      return -1;
+    }
+    else if (key < array[middle] && key >= left)  {
+      // console.log('less than');
+      right = middle;
+      middle = Math.floor((right-left)/2 + left); //(4 - 0)/2 + 0 = 3
+    }
+    else if (key > array[middle] && key <= right) {
+      // console.log('greater than');
+      left = middle;
+      middle = Math.ceil((right-left)/2 + left);
+    }
   }
-  return -1;
+  console.log('Ive been reached the middle is', middle);
+  return middle;
 };
 
 module.exports = binarySearch;
-
-
-let array = [1,5,10,15,20,25,30,35];
-let key = 20;
-
-binarySearch(array,key);
